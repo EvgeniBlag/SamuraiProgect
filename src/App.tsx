a8d58e915cb93a74436b1e5919cdfde53ea7147a
EvgeniBlag <blagodarni@gmail.com>
2022-04-04 16:15:01 +0200

    inishon progect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go
+    <div className="app-wrapper">
+
+      <Header />
+      <Navbar />
+      <Profile />
+
     </div>
   );
 }
